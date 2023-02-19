fe3626c19f9b3d08747d02e05e6f4fa72d81cc09
lhchan4010 <lhchan4010@gmail.com>
2023-02-20 06:40:44 +0900

    apollo playground

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 import { ApolloServer } from "@apollo/server";
+import {
+    ApolloServerPluginLandingPageLocalDefault,
+    ApolloServerPluginLandingPageProductionDefault,
+} from "@apollo/server/plugin/landingPage/default";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { PrismaClient } from "@prisma/client";
 import { typeDefs, resolvers } from "./schema";
@@ -8,6 +12,14 @@ export const client = new PrismaClient();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [
+        process.env.NODE_ENV === "production"
+            ? ApolloServerPluginLandingPageProductionDefault({
+                  graphRef: "my-graph-id@my-graph-variant",
+                  footer: false,
+              })
+            : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    ],
 });
 
 const startServer = async () => {
