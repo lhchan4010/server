fd5e299b214279dd213b33061ee60cba0e607419
lhchan4010 <lhchan4010@gmail.com>
2023-02-28 16:36:46 +0900

    seeProfile resolver await error fix

--- src/user/user.resolvers.js
+++ src/user/user.resolvers.js
@@ -2,8 +2,8 @@ import { client } from "../client";
 
 export default {
     Query: {
-        seeProfile: (_, { email }) => {
-            const user = client.user.findUnique({
+        seeProfile: async (_, { email }) => {
+            const user = await client.user.findUnique({
                 where: { email },
             });
             return user;
